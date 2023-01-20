22315e6e2fcb9930d210f1b129d65b8f6f5a6d93
Артем Жильцов <art.zhiltsoff@gmail.com>
2023-01-20 18:19:20 +0300

    added example data

--- src/interfaces/example.ts
+++ src/interfaces/example.ts
@@ -1,5 +1,19 @@
 import { FORM_GROUP_DIRECTION, IForm } from './form-config';
 import { CONTROL_TYPE } from './form-control';
+import { IParameter, PARAMETER_TYPE } from './parameter';
+
+export const parameters: IParameter[] = [
+  {
+    code: 'Parameter1',
+    name: 'Параметр 1',
+    type: PARAMETER_TYPE.NUMBER,
+  },
+  {
+    code: 'Parameter2',
+    name: 'Параметр 2',
+    type: PARAMETER_TYPE.STRING,
+  }
+]
 
 /** Пример объекта. */
 export const form: IForm = {
@@ -26,19 +40,23 @@ export const form: IForm = {
                   dataSource: 'Parameter1',
                   type: CONTROL_TYPE.NUMBER,
                   properties: {
-                    boolProperty: true,
-                    stringProperty: 'Строковое значение',
-                    numberProperty: 1,
-                    nullProperty: null,
+                    minValue: 0,
+                    maxValue: 100,
+                  },
+                },
+                {
+                  code: 'Element2',
+                  name: 'Элемент2',
+                  dataSource: 'Parameter2',
+                  type: CONTROL_TYPE.TEXT,
+                  properties: {
+                    multiline: true,
+                    lineCount: 2,
                   },
                 },
               ],
             },
           ],
-        },
-        {
-          code: 'Page1',
-          name: 'Страница 1',
         },
       ],
     },
