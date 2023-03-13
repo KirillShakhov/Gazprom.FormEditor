a46d5d6a97fa7d70a2b510dfb5a5c001f563ed96
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-03-13 15:24:16 +0300

    refactor: LeftMenu

--- src/components/form-editor/index.tsx
+++ src/components/form-editor/index.tsx
@@ -5,13 +5,9 @@ import './style.css';
 import { VisualMode } from '../visual-mode';
 import { TextMode } from '../text-mode/text-mode';
 import { ComponentSettings } from '../component-settings';
-import {ParametersTab} from "../parameters-tab";
+import {ParametersTab} from "../left-menu/parameters-tab";
+import {LeftMenu} from "../left-menu";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
 
 enum Modes {
   Visual,
@@ -20,46 +16,11 @@ enum Modes {
 
 /** Редактор форм. */
 export const FormEditor: React.FC = () => {
-  const [value, setValue] = React.useState(0);
   const [mode, setMode] = React.useState(Modes.Text);
-  const [lists, setLists] = useState(['banana', 'hogehoge', 'tomato']);
-
-  const tabStyle = {
-    minWidth: 20,
-    paddingLeft: 10,
-    paddingRight: 10,
-    backgroundColor: 'white',
-  };
 
   const standardData =
     '{"glossary":{"title":"example glossary","GlossDiv":{"title":"S","GlossList":{"GlossEntry":{"ID":"SGML","SortAs":"SGML","GlossTerm":"Standard Generalized Markup Language","Acronym":"SGML","Abbrev":"ISO 8879:1986","GlossDef":{"para":"A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso":["GML","XML"]},"GlossSee":"markup"}}}}}';
   const [data, setData] = React.useState(standardData);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {children}
-      </div>
-    );
-  }
-
-  function a11yProps(index: number) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
 
   const changeMode = () => {
     if (mode === Modes.Text) {
@@ -86,22 +47,7 @@ export const FormEditor: React.FC = () => {
         </CommandLine>
       </header>
       <div className="left-side">
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange}>
-            <Tab label="Параметры" {...a11yProps(0)} style={tabStyle} />
-            <Tab label="Форма" {...a11yProps(1)} style={tabStyle} />
-            <Tab label="Компоненты" {...a11yProps(2)} style={tabStyle} />
-          </Tabs>
-        </Box>
-        <TabPanel value={value} index={0}>
-          <ParametersTab lists={lists} setLists={setLists}></ParametersTab>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <div className="red">Item Three</div>
-        </TabPanel>
+        <LeftMenu></LeftMenu>
       </div>
       <main>
         <div hidden={mode != Modes.Visual}>
---
+++ src/components/left-menu/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import './style.css';
+import { Box, Tab, Tabs } from '@mui/material';
+import { ParametersTab } from './parameters-tab';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const tabStyle = {
+  minWidth: 20,
+  paddingLeft: 10,
+  paddingRight: 10,
+  backgroundColor: 'white',
+};
+
+export const LeftMenu: React.FC = () => {
+  const [value, setValue] = React.useState(0);
+  const [lists, setLists] = useState(['banana', 'hogehoge', 'tomato']);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {children}
+      </div>
+    );
+  }
+
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+  return (
+    <div className="left-menu">
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Параметры" {...a11yProps(0)} style={tabStyle} />
+          <Tab label="Форма" {...a11yProps(1)} style={tabStyle} />
+          <Tab label="Компоненты" {...a11yProps(2)} style={tabStyle} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <ParametersTab lists={lists} setLists={setLists}></ParametersTab>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <div className="red">Item Three</div>
+      </TabPanel>
+    </div>
+  );
+};
