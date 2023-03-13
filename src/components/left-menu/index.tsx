b8c695b81031718db221491046877add68efe4b2
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-03-13 16:21:16 +0300

    feat: LeftMenu style

--- src/components/left-menu/index.tsx
+++ src/components/left-menu/index.tsx
@@ -10,13 +10,6 @@ interface TabPanelProps {
   value: number;
 }
 
-const tabStyle = {
-  minWidth: 20,
-  paddingLeft: 10,
-  paddingRight: 10,
-  backgroundColor: 'white',
-};
-
 export const LeftMenu: React.FC = () => {
   const [value, setValue] = React.useState(1);
   const [lists, setLists] = useState(['banana', 'hogehoge', 'tomato']);
@@ -50,10 +43,24 @@ export const LeftMenu: React.FC = () => {
     };
   }
 
+  const tabStyle = {
+    minWidth: 20,
+    minHeight: 30,
+    height: 30,
+    padding: 5,
+    fontSize: 12,
+  };
+
   return (
     <div className="left-menu">
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          style={{
+            minHeight: 0,
+          }}
+        >
           <Tab label="Параметры" {...a11yProps(0)} style={tabStyle} />
           <Tab label="Форма" {...a11yProps(1)} style={tabStyle} />
           <Tab label="Компоненты" {...a11yProps(2)} style={tabStyle} />
