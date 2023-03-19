77466b5bc72bab37698205491a1d182ddfcce60a
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-03-19 19:43:37 +0300

    feat: many types

--- src/components/form-editor/visual-mode/element/index.tsx
+++ src/components/form-editor/visual-mode/element/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { CONTROL_TYPE, IFormControl } from '../../../../interfaces/form-control';
-import { DefaultType } from './types/default-type';
-import { NumberType } from './types/number-type';
-import { TextType } from './types/text-type';
+import {CONTROL_TYPE, IFormControl} from '../../../../interfaces/form-control';
+import {DefaultType} from './types/default-type';
+import {NumberType} from './types/number-type';
+import {TextType} from './types/text-type';
 import {TextAreaType} from "./types/textarea-type";
+import {DatePickerType} from "./types/datepicker-type";
+import {SwitchType} from "./types/switch-type";
+import {CheckBoxType} from "./types/checkbox-type";
+import {RadioGroupType} from "./types/radiogroup-type";
 
 interface ElementProps {
   value: IFormControl;
@@ -17,6 +21,14 @@ function renderSwitch(value: IFormControl) {
       return <TextType value={value}></TextType>;
     case CONTROL_TYPE.TEXTAREA:
       return <TextAreaType value={value}></TextAreaType>;
+    case CONTROL_TYPE.DATEPICKER:
+      return <DatePickerType value={value}></DatePickerType>;
+    case CONTROL_TYPE.SWITCH:
+      return <SwitchType value={value}></SwitchType>;
+    case CONTROL_TYPE.CHECKBOX:
+      return <CheckBoxType value={value}></CheckBoxType>;
+    case CONTROL_TYPE.RADIOGROUP:
+      return <RadioGroupType value={value}></RadioGroupType>;
     default:
       return <DefaultType value={value}></DefaultType>;
   }
