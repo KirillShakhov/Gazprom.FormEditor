6699d9873bddfde4214ad085b8e66ab3f1fc77ef
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-04-06 01:08:28 +0300

    feat: type helpers

--- src/components/form-editor/visual-mode/element/types/number-type.tsx
+++ src/components/form-editor/visual-mode/element/types/number-type.tsx
@@ -18,19 +18,33 @@ export const NumberType: React.FC<ElementProps> = (props) => {
   };
 
   return (
-    <TextField
-      label={value.name}
-      onChange={handleChange}
-      name={value.name}
-      id={value.code}
-      size={'small'}
-      variant="outlined"
-      // InputProps={{
-      //   inputComponent: NumericFormat as never,
-      // }}
-      value={'Значение'}
-      disabled
-      fullWidth
-    />
+    <div hidden={value.properties && value.properties.hasOwnProperty('hidden')}>
+      <TextField
+        label={value.name}
+        onChange={handleChange}
+        name={value.name}
+        id={value.code}
+        size={'small'}
+        variant="outlined"
+        // InputProps={{
+        //   inputComponent: NumericFormat as never,
+        // }}
+        value={'Значение'}
+        disabled
+        fullWidth
+      />
+      {value.properties && value.properties.hasOwnProperty('hint') && (
+        <div
+          style={{
+            fontSize: 12,
+            marginTop: 5,
+            marginLeft: 5,
+            color: '#9b9b9b',
+          }}
+        >
+          {value.properties['hint']}
+        </div>
+      )}
+    </div>
   );
 };
--- src/components/form-editor/visual-mode/element/types/text-type.tsx
+++ src/components/form-editor/visual-mode/element/types/text-type.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { IFormControl } from '../../../../../interfaces/form-control';
 import { TextField } from '@mui/material';
 
@@ -10,15 +10,41 @@ export const TextType: React.FC<ElementProps> = (props) => {
   const { value } = props;
 
   return (
-    <TextField
-      label={value.name}
-      name={value.name}
-      id={value.code}
-      size={'small'}
-      variant="outlined"
-      value={'Значение'}
-      disabled
-      fullWidth
-    />
+    <div hidden={value.properties && value.properties.hasOwnProperty('hidden')}>
+      <TextField
+        label={value.name}
+        name={value.name}
+        id={value.code}
+        size={'small'}
+        variant="outlined"
+        value={'Значение'}
+        disabled
+        fullWidth
+      />
+      {value.properties && value.properties.hasOwnProperty('hint') && (
+        <div
+          style={{
+            fontSize: 12,
+            marginTop: 5,
+            marginLeft: 5,
+            color: '#9b9b9b',
+          }}
+        >
+          {value.properties['hint']}
+        </div>
+      )}
+      {value.properties && value.properties.hasOwnProperty('counter') && (
+        <div
+          style={{
+            fontSize: 12,
+            marginTop: 5,
+            marginLeft: 5,
+            color: '#9b9b9b',
+          }}
+        >
+          Количество символов: 0
+        </div>
+      )}
+    </div>
   );
 };
