70f44dfd6832a2d2421dfeecf703d0942e759efd
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-04-13 08:51:57 +0300

    feat: component settings for pages, page, group

--- src/components/form-editor/component-settings/types/component-setting-properties.tsx
+++ src/components/form-editor/component-settings/types/component-setting-properties.tsx
@@ -3,10 +3,11 @@ import { TextField } from '@mui/material';
 import { CONTROL_TYPE, IFormControl } from '../../../../interfaces/form-control';
 import { IPropertyConfig, IPropertyMetadata } from '../../../../interfaces/property-metadata';
 import { PropertyConfig } from './property-config';
-import { isFormControl } from '../../../../utils/form-config';
+import { isFormControl, isFormGroup, isFormPage, isTabPageController } from '../../../../utils/form-config';
+import { IFormElement } from '../../../../interfaces/form-element';
 
 interface ElementProps {
-  formItem: IFormControl;
+  formItem: IFormElement;
   config: IPropertyMetadata;
   update: () => void;
 }
@@ -24,71 +25,95 @@ export const ComponentSettingProperties: React.FC<ElementProps> = (props) => {
     update();
   };
 
-  const getSettingComponent = (value: IFormControl): IPropertyConfig[] | undefined => {
-    switch (value.type) {
-      case CONTROL_TYPE.TEXT:
-        return config.byParameterType.STRING;
-      case CONTROL_TYPE.NUMBER:
-        return config.byParameterType.NUMBER;
-      case CONTROL_TYPE.CHECKBOX:
-        return config.byParameterType.BOOLEAN;
-      case CONTROL_TYPE.SWITCH:
-        return config.byParameterType.BOOLEAN;
-      case CONTROL_TYPE.DATEPICKER:
-        return config.byParameterType.DATE;
-      case CONTROL_TYPE.DATETIMEPICKER:
-        return config.byParameterType.DATETIME;
-      case CONTROL_TYPE.LINK:
-        return config.byParameterType.REF;
-      case CONTROL_TYPE.FILE:
-        return config.byParameterType.FILE;
-      case CONTROL_TYPE.TEXTAREA:
-        return config.byParameterType.STRING;
-      case CONTROL_TYPE.RADIOGROUP:
-        return config.byParameterType.REF;
-      case CONTROL_TYPE.COMBOBOX:
-        return config.byParameterType.REF;
-      case CONTROL_TYPE.SELECT:
-        return config.byParameterType.REF;
-      default:
-        return undefined;
-    }
-  };
+  const getSettingComponent = useCallback(
+    (value: IFormControl): IPropertyConfig[] | undefined => {
+      switch (value.type) {
+        case CONTROL_TYPE.TEXT:
+          return config.byParameterType.STRING;
+        case CONTROL_TYPE.NUMBER:
+          return config.byParameterType.NUMBER;
+        case CONTROL_TYPE.CHECKBOX:
+          return config.byParameterType.BOOLEAN;
+        case CONTROL_TYPE.SWITCH:
+          return config.byParameterType.BOOLEAN;
+        case CONTROL_TYPE.DATEPICKER:
+          return config.byParameterType.DATE;
+        case CONTROL_TYPE.DATETIMEPICKER:
+          return config.byParameterType.DATETIME;
+        case CONTROL_TYPE.LINK:
+          return config.byParameterType.REF;
+        case CONTROL_TYPE.FILE:
+          return config.byParameterType.FILE;
+        case CONTROL_TYPE.TEXTAREA:
+          return config.byParameterType.STRING;
+        case CONTROL_TYPE.RADIOGROUP:
+          return config.byParameterType.REF;
+        case CONTROL_TYPE.COMBOBOX:
+          return config.byParameterType.REF;
+        case CONTROL_TYPE.SELECT:
+          return config.byParameterType.REF;
+        default:
+          return undefined;
+      }
+    },
+    [config],
+  );
 
-  const getTextLabel = (value: IFormControl): string => {
-    switch (value.type) {
-      case CONTROL_TYPE.TEXT:
-        return 'Text Input';
-      case CONTROL_TYPE.NUMBER:
-        return 'Number  Input';
-      case CONTROL_TYPE.CHECKBOX:
-        return 'Checkbox';
-      case CONTROL_TYPE.SWITCH:
-        return 'Switch';
-      case CONTROL_TYPE.DATEPICKER:
-        return 'DatePicker';
-      case CONTROL_TYPE.DATETIMEPICKER:
-        return 'DateTimePicker';
-      case CONTROL_TYPE.LINK:
-        return 'Link';
-      case CONTROL_TYPE.FILE:
-        return 'File';
-      case CONTROL_TYPE.TEXTAREA:
-        return 'TextArea';
-      case CONTROL_TYPE.RADIOGROUP:
-        return 'RadioGroup';
-      case CONTROL_TYPE.COMBOBOX:
-        return 'ComboBox';
-      case CONTROL_TYPE.SELECT:
-        return 'Select';
-      default:
-        return 'undefined';
+  const getTextLabel = (value: IFormElement): string => {
+    if (isFormControl(value)) {
+      switch (value.type) {
+        case CONTROL_TYPE.TEXT:
+          return 'Text Input';
+        case CONTROL_TYPE.NUMBER:
+          return 'Number  Input';
+        case CONTROL_TYPE.CHECKBOX:
+          return 'Checkbox';
+        case CONTROL_TYPE.SWITCH:
+          return 'Switch';
+        case CONTROL_TYPE.DATEPICKER:
+          return 'DatePicker';
+        case CONTROL_TYPE.DATETIMEPICKER:
+          return 'DateTimePicker';
+        case CONTROL_TYPE.LINK:
+          return 'Link';
+        case CONTROL_TYPE.FILE:
+          return 'File';
+        case CONTROL_TYPE.TEXTAREA:
+          return 'TextArea';
+        case CONTROL_TYPE.RADIOGROUP:
+          return 'RadioGroup';
+        case CONTROL_TYPE.COMBOBOX:
+          return 'ComboBox';
+        case CONTROL_TYPE.SELECT:
+          return 'Select';
+        default:
+          return 'undefined';
+      }
     }
+    if (isFormGroup(value)) {
+      return 'Group';
+    }
+    if (isTabPageController(value)) {
+      return 'Page Group';
+    }
+    if (isFormPage(value)) {
+      return 'Page';
+    }
+    return 'undefined';
   };
 
   const getAllProperties = useCallback(() => {
     const list: IPropertyConfig[] = [];
     list.push(...(config.byElementType.ELEMENT ?? []));
+    if (isTabPageController(formItem)) {
+      list.push(...(config.byElementType.PAGES ?? []));
+    }
+    if (isFormPage(formItem)) {
+      list.push(...(config.byElementType.PAGE ?? []));
+    }
+    if (isFormGroup(formItem)) {
+      list.push(...(config.byElementType.GROUP ?? []));
+    }
     if (isFormControl(formItem)) {
       list.push(...(config.byElementType.CONTROL ?? []));
       list.push(...(getSettingComponent(formItem) ?? []));
--- src/utils/form-config.ts
+++ src/utils/form-config.ts
@@ -1,4 +1,4 @@
-import { IFormGroup, IFormItem, ITabPageController } from '../interfaces/form-config';
+import { IFormGroup, ITabPage, ITabPageController } from '../interfaces/form-config';
 import { IFormControl } from '../interfaces/form-control';
 import { IFormElement } from '../interfaces/form-element';
 
@@ -8,8 +8,13 @@ import { IFormElement } from '../interfaces/form-element';
 export const isTabPageController = (item: IFormElement): item is ITabPageController =>
   (item as ITabPageController).pages !== undefined;
 
-/** Явяляется ли элемент группой. */
+/** Является ли элемент группой. */
 export const isFormGroup = (item: IFormElement): item is IFormGroup => (item as IFormGroup).direction !== undefined;
+
+/** Является ли элемент страницей. */
+export const isFormPage = (item: IFormElement): item is ITabPage => {
+  return !item.hasOwnProperty('direction') && (item as ITabPage).items !== undefined;
+};
 
 /** Является ли элемент полем ввода. */
 export const isFormControl = (item: IFormElement): item is IFormControl =>
